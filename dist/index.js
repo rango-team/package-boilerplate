d80dad87d838ae004e992ab13355687e2db1f886
Gabriel <gabrielmarques.mg@gmail.com>
2022-09-06 17:59:13 -0300

    :fire: Remove build folder and files

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sayHello = void 0;
-const sayHello = () => {
-    return 'Hello :D';
-};
-exports.sayHello = sayHello;
-//# sourceMappingURL=index.js.map
