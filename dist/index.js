890b948e692ed919605a643e59f934f2979c5a32
Gabriel <gabrielmarques.mg@gmail.com>
2022-09-08 12:06:15 -0300

    :see_no_evil: Removing build folder

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayHello = void 0;
+const sayHello = () => {
+    return 'Hello :D';
+};
+exports.sayHello = sayHello;
+//# sourceMappingURL=index.js.map
